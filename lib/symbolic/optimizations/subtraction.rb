af2befc6b627335877ad73fad9e43444e6b0bc38
brainopia <ravwar@gmail.com>
2009-11-28 21:55:00 +0300

    Dont use -x-y = -(x+y) optimization

--- lib/symbolic/optimizations/subtraction.rb
+++ lib/symbolic/optimizations/subtraction.rb
@@ -2,15 +2,14 @@ module Symbolic::Optimizations::Subtraction
   extend Symbolic::Optimizations::Base
 
   def self.optimize_first_arg(var1, var2)
-    if var1 == 0
-      -var2
-    elsif negative?(var1)
-      -(var1.abs + var2)
-    end
+    -var2 if var1 == 0
   end
 
   def self.optimize_second_arg(var1, var2)
-    reverse_optimization = optimize_first_arg(var2, var1)
-    -reverse_optimization if reverse_optimization
+    if var2 == 0
+      var1
+    elsif negative?(var2)
+      var1 + var2.abs
+    end
   end
 end
