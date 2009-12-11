ae9f0dab871f2db419cdbf40e223635a539513ba
brainopia <ravwar@gmail.com>
2009-12-11 15:18:54 +0300

    Fix bug in Function#value

--- lib/symbolic/function.rb
+++ lib/symbolic/function.rb
@@ -1,28 +1,27 @@
-module Symbolic
-  class Function
-    include Symbolic
-    def initialize(variable, operation)
-      @variable, @operation = variable, operation
-    end
+class Symbolic::Function
+  include Symbolic
 
-    def to_s
-      "#{@operation}(#{@variable})"
-    end
+  def initialize(variable, operation)
+    @variable, @operation = variable, operation
+  end
 
-    def value
-      Math.send @operation, @variable.value if undefined_variables.empty?
-    end
+  def to_s
+    "#{@operation}(#{@variable})"
+  end
 
-    def variables
-      @variable.variables
-    end
+  def value
+    Math.send @operation, @variable.value if undefined_variables.empty?
+  end
 
-    def undefined_variables
-      @variable.undefined_variables
-    end
+  def variables
+    @variable.variables
+  end
 
-    def detailed_operations
-      @variable.detailed_operations.tap {|it| it[@operation] += 1}
-    end
+  def undefined_variables
+    @variable.undefined_variables
+  end
+
+  def detailed_operations
+    @variable.detailed_operations.tap {|it| it[@operation] += 1}
   end
 end
