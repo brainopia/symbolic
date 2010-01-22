6ae5b6785354c27af5f2fe25d547bc8f716a6a48
eregon <eregontp@gmail.com>
2010-01-22 13:27:49 +0100

    bad indentation

--- lib/symbolic/variable.rb
+++ lib/symbolic/variable.rb
@@ -1,30 +1,30 @@
 module Symbolic
 =begin
-  This class is used to create symbolic variables.
-  Symbolic variables presented by name and value.
-  Name is neccessary for printing meaningful symbolic expressions.
-  Value is neccesary for calculation of symbolic expressions.
-  If value isn't set for variable, but there is an associated proc, then value is taken from evaluating the proc.
+This class is used to create symbolic variables.
+Symbolic variables presented by name and value.
+Name is neccessary for printing meaningful symbolic expressions.
+Value is neccesary for calculation of symbolic expressions.
+If value isn't set for variable, but there is an associated proc, then value is taken from evaluating the proc.
 =end
   class Variable
     include Symbolic
     attr_accessor :name, :proc
     attr_writer :value
 
-		# Create a new Symbolic::Variable, with optional name, value and proc
-		def initialize(*args, &proc)
-			args.each do |arg|
-				case arg
-				when Numeric then @value = arg
-				when String, Symbol then @name = arg.to_s
-				else raise ArgumentError, "Bad argument(String|Symbol name, Numeric value, Proc proc) : #{arg}(#{arg.class})"
-				end
-			end
-			@proc = proc
-		end
+    # Create a new Symbolic::Variable, with optional name, value and proc
+    def initialize(*args, &proc)
+      args.each do |arg|
+        case arg
+        when Numeric then @value = arg
+        when String, Symbol then @name = arg.to_s
+        else raise ArgumentError, "Bad argument(String|Symbol name, Numeric value, Proc proc) : #{arg}(#{arg.class})"
+        end
+      end
+      @proc = proc
+    end
 
     def value
-			instance_variable_defined?(:@value) ? @value : @proc && @proc.call.value
+      instance_variable_defined?(:@value) ? @value : @proc && @proc.call.value
     end
 
     def to_s
