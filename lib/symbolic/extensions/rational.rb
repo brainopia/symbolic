f40c4412a451a899749842594b39ed498663b77c
eregon <eregontp@gmail.com>
2010-01-26 21:24:58 +0100

    Fix for rational.rb, a real one ;)

--- lib/symbolic/extensions/rational.rb
+++ lib/symbolic/extensions/rational.rb
@@ -1,16 +1,17 @@
 # Fix for rational on 1.8.7
 if RUBY_VERSION == '1.8.7'
+  require 'rational'
   [Fixnum,Bignum].each do |klass|
     klass.class_eval do
+      alias :old_power :**
       # this method is copy-pasted from ruby std, the only change is conditional for Numeric
-      def rpower(other)
+      def **(other)
         if other.is_a?(Numeric) && other < 0
           Rational.new!(self, 1)**other
         else
-          self.power!(other)
+          power!(other)
         end
       end
-      alias ** rpower
     end # class_eval
   end # each
 end # if
