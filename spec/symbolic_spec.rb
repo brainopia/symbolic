ee8e0a4b6f14eed1fd8f261bcc2a29cfdcffd913
brainopia <ravwar@gmail.com>
2010-02-15 19:10:12 +0300

    A few more specs for Symbolic module

--- spec/symbolic_spec.rb
+++ spec/symbolic_spec.rb
@@ -14,4 +14,39 @@ describe Symbolic do
     Symbolic::Factors.should_receive(:add).with(@var, -1).and_return(:negative_value)
     (-@var).should == :negative_value
   end
+
+  it 'should handle binary plus' do
+    Symbolic::Summands.should_receive(:add).with(@var, :other_var).and_return(:result)
+    (@var + :other_var).should == :result
+  end
+
+  it 'should handle binary minus' do
+    Symbolic::Summands.should_receive(:subtract).with(@var, :other_var).and_return(:result)
+    (@var - :other_var).should == :result
+  end
+
+  it 'should handle multiplication' do
+    Symbolic::Factors.should_receive(:add).with(@var, :other_var).and_return(:result)
+    (@var * :other_var).should == :result
+  end
+
+  it 'should handle division' do
+    Symbolic::Factors.should_receive(:subtract).with(@var, :other_var).and_return(:result)
+    (@var / :other_var).should == :result
+  end
+
+  it 'should handle exponentation' do
+    Symbolic::Factors.should_receive(:power).with(@var, :other_var).and_return(:result)
+    (@var ** :other_var).should == :result
+  end
+
+  it 'should delegate string representation to Printer' do
+    Symbolic::Printer.should_receive(:print).with(@var).and_return(:string)
+    @var.to_s.should == :string
+  end
+
+  it 'should redefine inspect' do
+    @var.should_receive(:to_s).and_return(:string)
+    @var.inspect.should == "Symbolic(string)"
+  end
 end
