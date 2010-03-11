81c977b0bfe38e0cd32d0907d1ca74706eebd5f1
Leon N. Maurer <leon@leon-laptop.(none)>
2010-03-11 11:13:02 -0600

    Inital refactoring of Funcion class.

--- lib/symbolic/function.rb
+++ lib/symbolic/function.rb
@@ -3,10 +3,23 @@ class Symbolic::Function
   This class is proxy for methods from Math module.
 =end 
   include Symbolic
-  attr_reader :argument, :name, :operation
-  attr_accessor :deriv, :argument
-  def initialize(argument, name, deriv = nil, &block)
-    @argument, @name, @deriv, @operation = argument, name, deriv, block
+  attr_reader :argument, :name, :derivative, :operation
+  
+  #deriv can be either a function or a proc that takes an argument
+  #operation can be either passed as a block or proc
+  def initialize(argument, name, deriv = nil, op = nil, &block)
+    @argument, @name, @derivative = argument, name, deriv
+    unless op == nil
+      @operation = op
+    else
+      @operation = block
+    end
+  end
+  
+  #it may be easier to set the derivitve after the object is made
+  def set_derivative(deriv)
+    #only allow it to be set if @derivative is nil
+    @derivative = deriv if @derivative == nil
   end
 
   def value
@@ -21,41 +34,27 @@ class Symbolic::Function
     @argument.detailed_operations.tap {|it| it[@operation] += 1}
   end
 
-  def subs(to_replace, replacement)
-    new_arg = @argument.subs(to_replace, replacement)
+  #returns a function of the same type, but with arg replaced by new_arg
+  def [] (new_arg)
     if new_arg.is_a?(::Numeric)
-      @operation.cal(new_arg)
+      @operation.call(new_arg)
     else
-      Function.new(new_arg, @operation)
-    end  
+      Function.new(new_arg, @name, @derivative, @operation)
+    end
+  end
+  
+  def subs(to_replace, replacement)
+    self[@argument.subs(to_replace, replacement)]
   end
   
   def diff(wrt)
     return 0 unless self.variables.member?(wrt)
-    if @deriv != nil
-    #chain rule
-    Function.operator_derivative(@name, @argument) * @argument.diff(wrt)
-  end
-  
-  def Function.operator_derivative(name,arg)
-    case name
-    when 'sqrt' then Rational(1,2) / arg ** Rational(1,2) #could keep it in terms of sqrt
-    when 'exp' then Symbolic::Math.exp(arg)
-    when 'log' then 1 / arg
-    when 'log10' then 1 / arg / Symbolic::Math.log(10) #since log10(x) = log(x) / log(10)
-    when 'cos' then - Symbolic::Math.sin(arg)
-    when 'sin' then Symbolic::Math.cos(arg)
-    when 'tan' then 1 / Symbolic::Math.cos(arg) ** 2
-    when 'cosh' then Symbolic::Math.sinh(arg) #no negative sign
-    when 'sinh' then Symbolic::Math.cosh(arg)
-    when 'tanh' then 1 / Symbolic::Math.cosh(arg) ** 2
-    when 'acos' then - 1 / (1 - arg) ** Rational(1,2)
-    when 'asin' then 1 / (1 - arg) ** Rational(1,2)
-    when 'atan' then 1/ (arg**2 + 1)
-#     when :atan2 then
-    when 'acosh' then 1 / (1 - arg) ** Rational(1,2)
-    when 'asinh' then 1 / (1 + arg) ** Rational(1,2)
-    when 'atanh' then 1/ (1 - arg**2)
+    #TODO: raise exception if derivative == nil
+    if @derivative.is_a?(Proc)
+      #chain rule
+      @derivative.call(@argument) * @argument.diff(wrt)
+    else #it's a function
+      @derivative[@argument] * @argument.diff(wrt)
     end
   end
 end
--- lib/symbolic/math.rb
+++ lib/symbolic/math.rb
@@ -2,18 +2,68 @@ module Symbolic::Math
 =begin
   This module is a reflection for Math module which allows to use symbolic expressions as parameters for its methods.
 =end
-  Math.methods(false).each do |method|
-    instance_eval <<-CODE, __FILE__, __LINE__ + 1
-      def #{method}(argument)
-        unless argument.is_a? Numeric
-          Symbolic::Function.new(argument,#{method}){|x| ::Math.#{method}(x)}
-        else
-          ::Math.#{method}(argument)
-        end
-      end
-    CODE
+  
+  require '/home/leon/Development/symbolic/lib/symbolic/function.rb'
+  
+  #sqrt = Symbolic::Function.new(nil,'sqrt', proc{|arg| Rational(1,2) / arg ** Rational(1,2)}) {|arg| ::Math.sqrt(arg)}
+  def sqrt(arg)
+    #sqrt[arg]
+    arg ** Rational(1,2)
   end
-  def hstep
-    
+  exp = Symbolic::Function.new(nil,'exp',nil) {|arg| ::Math.exp(arg)}
+  exp.set_derivative(exp)
+  def exp(arg)
+    exp[arg]
   end
+  log = Symbolic::Function.new(nil,'log', proc{|arg| 1 / arg}) {|arg| ::Math.log(arg)}
+  def log(arg)
+    log[arg]
+  end
+  log10 = Symbolic::Function.new(nil,'log10', proc{|arg| 1 / arg / ::Math.log(10)}) {|arg| ::Math.log10(arg)} #since log10(x) = log(x) / log(10)
+  def log10(arg)
+    log10[arg]
+  end
+  cos = Symbolic::Function.new(nil,'cos',nil) {|arg| ::Math.cos(arg)}
+  sin = Symbolic::Function.new(nil,'sin',cos) {|arg| ::Math.sin(arg)}
+  cos.set_derivative(proc{|arg| - cos[arg]})
+  def cos(arg)
+    cos[arg]
+  end
+  def sin(arg)
+    sin[arg]
+  end
+  tan = Symbolic::Function.new(nil,'tan', proc{|arg| 1 / cos[arg] ** 2}) {|arg| ::Math.tan(arg)}
+#     when 'cosh' then Symbolic::Math.sinh(arg) #no negative sign
+#     when 'sinh' then Symbolic::Math.cosh(arg)
+#     when 'tanh' then 1 / Symbolic::Math.cosh(arg) ** 2
+#     when 'acos' then - 1 / (1 - arg) ** Rational(1,2)
+#     when 'asin' then 1 / (1 - arg) ** Rational(1,2)
+#     when 'atan' then 1/ (arg**2 + 1)
+#     when 'acosh' then 1 / (1 - arg) ** Rational(1,2)
+#     when 'asinh' then 1 / (1 + arg) ** Rational(1,2)
+#     when 'atanh' then 1/ (1 - arg**2)
+
+  
+  
+#     def Function.operator_derivative(name,arg)
+#     case name
+#     when 'sqrt' then Rational(1,2) / arg ** Rational(1,2) #could keep it in terms of sqrt
+#     when 'exp' then Symbolic::Math.exp(arg)
+#     when 'log' then 1 / arg
+#     when 'log10' then 1 / arg / Symbolic::Math.log(10) #since log10(x) = log(x) / log(10)
+#     when 'cos' then - Symbolic::Math.sin(arg)
+#     when 'sin' then Symbolic::Math.cos(arg)
+#     when 'tan' then 1 / Symbolic::Math.cos(arg) ** 2
+#     when 'cosh' then Symbolic::Math.sinh(arg) #no negative sign
+#     when 'sinh' then Symbolic::Math.cosh(arg)
+#     when 'tanh' then 1 / Symbolic::Math.cosh(arg) ** 2
+#     when 'acos' then - 1 / (1 - arg) ** Rational(1,2)
+#     when 'asin' then 1 / (1 - arg) ** Rational(1,2)
+#     when 'atan' then 1/ (arg**2 + 1)
+# #     when :atan2 then
+#     when 'acosh' then 1 / (1 - arg) ** Rational(1,2)
+#     when 'asinh' then 1 / (1 + arg) ** Rational(1,2)
+#     when 'atanh' then 1/ (1 - arg**2)
+#     end
+#   end
 end
