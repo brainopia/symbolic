4fe8fe2b4015918658cecd01795d6a1eefc7ca0a
eregon <eregontp@gmail.com>
2011-01-27 19:36:33 +0100

    define methods instead of using method_missing

--- new/symbolic/operators.rb
+++ new/symbolic/operators.rb
@@ -22,58 +22,59 @@ module Symbolic
       [Abelian.new(numeric), self]
     end
     
-    def unary_operation(op)
-      if op == :+@
-        self
-      else # -@
-        case self
-        when Factors
-          self << -1
-        else
-          Factors.new(self, -1)
-        end.optimized
-      end
+    def +@
+      self
     end
     
-    def binary_operation(op, o)
-      # TODO: call #optimized on result
-      group_class = OPERATORS_GROUPS[op]
-      group = (group_class === self) ? self : group_class.new(self)
-
-      # Basic simplifications
-      # if o == group.identity # + 0, - 0, * 1, / 1 is doing nothing
-      #   return self
-      # elsif Abelian === self and self.value == group.identity and ( !(Variable === self) )
-      #   if op == group.operation
-      #     # 0 +, 1 * is doing nothing
-      #     return o
-      #   elsif op == Operators.inverse(group.operation)
-      #     # 0 -, 1 /
-      #     return group.member_class.new(o, -1) # o.new { |b,p|  }
-      #   end
-      # end
-      return 1 if op == :** and (o == 0 or self == 1)
-
-      case op
-      when group.operation
-        group << o
-      when Operators.inverse(group.operation)
-        #raise
-        if op == :-
-          group << o * -1
-        else
-          group << Abelian.new(o, -1)
-        end
-      when :+, :* # Factors + o, Summands * o
-        group_class.new(group, o)
-      #when :-, :/ # Factors - o, Summands / o
-      #  group << Abelian.new(o, -1)
-      when :** # OPERATORS_RISING[group.operation]
-        group.renew { |b,p| [b, p*o] }
+    def -@
+      case self
+      when Factors
+        self << -1
       else
-        raise "#{self}(#{group}) #{op} #{o}"# group_class.new(group, o)
+        Factors.new(self, -1)
       end.optimized
     end
+    
+    BINARY_OPERATORS.each { |op|
+      define_method(op) { |o|
+        group_class = OPERATORS_GROUPS[op]
+        group = (group_class === self) ? self : group_class.new(self)
+
+        # Basic simplifications
+        # if o == group.identity # + 0, - 0, * 1, / 1 is doing nothing
+        #   return self
+        # elsif Abelian === self and self.value == group.identity and ( !(Variable === self) )
+        #   if op == group.operation
+        #     # 0 +, 1 * is doing nothing
+        #     return o
+        #   elsif op == Operators.inverse(group.operation)
+        #     # 0 -, 1 /
+        #     return group.member_class.new(o, -1) # o.new { |b,p|  }
+        #   end
+        # end
+        return 1 if op == :** and (o == 0 or self == 1)
+
+        case op
+        when group.operation
+          group << o
+        when Operators.inverse(group.operation)
+          #raise
+          if op == :-
+            group << o * -1
+          else
+            group << Abelian.new(o, -1)
+          end
+        when :+, :* # Factors + o, Summands * o
+          group_class.new(group, o)
+        #when :-, :/ # Factors - o, Summands / o
+        #  group << Abelian.new(o, -1)
+        when :** # OPERATORS_RISING[group.operation]
+          group.renew { |b,p| [b, p*o] }
+        else
+          raise "#{self}(#{group}) #{op} #{o}"# group_class.new(group, o)
+        end.optimized
+      }
+    }
 
     def method_missing(op, *args, &block)
       result = if UNARY_OPERATORS.include?(op) and args.length == 0
