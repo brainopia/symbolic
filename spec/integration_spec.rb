22fd1b3088e0044f5a6aaf83e37391daf6d7e15a
eregon <eregontp@gmail.com>
2011-01-27 18:06:10 +0100

    Update specs to RSpec 2 and clean a few examples
    * Sorry for the horrible diff
    * But you can see better with git diff --color-words=.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,150 +1,129 @@
 #!/usr/bin/env ruby -w
 require File.expand_path('../spec_helper', __FILE__)
 
-describe "Symbolic" do
-  before(:all) do
-    @x = var :name => :x, :value => 1
-    @y = var :name => :y, :value => 2
-  end
-
-  def expression(string)
-    eval string.gsub(/[xy]/, '@\0')
-  end
-
-  def self.should_equal(conditions)
-    conditions.each do |non_optimized, optimized|
-      it non_optimized do
-        expression(non_optimized).should == expression(optimized)
-      end
-    end
-  end
-
-  def self.should_evaluate_to(conditions)
-    conditions.each do |symbolic_expression, result|
-      it symbolic_expression do
-        expression(symbolic_expression).value.should == result
-      end
-    end
-  end
-
-  def self.should_print(conditions)
-    conditions.each do |symbolic_expression, result|
-      it symbolic_expression do
-        expression(symbolic_expression).to_s.should == result
-      end
-    end
-  end
-
+describe Symbolic do
   describe "evaluation (x=1, y=2):" do
-    should_evaluate_to \
-    'x'         => 1,
-    'y'         => 2,
-    '+x'        => 1,
-    '-x'        => -1,
-    'x + 4'     => 5,
-    '3 + x'     => 4,
-    'x + y'     => 3,
-    'x - 1'     => 0,
-    '1 - x'     => 0,
-    'x - y'     => -1,
-    '-x + 3'    => 2,
-    '-y - x'    => -3,
-    'x*3'       => 3,
-    '4*y'       => 8,
-    '(+x)*(-y)' => -2,
-    'x/2'       => 0.5,
-    'y/2'       => 1,
-    '-2/x'      => -2,
-    '4/(-y)'    => -2,
-    'x**2'      => 1,
-    '4**y'      => 16,
-    'y**x'      => 2
+    x = var :name => :x, :value => 1
+    y = var :name => :y, :value => 2
+    {
+      x         => 1,
+      y         => 2,
+      +x        => 1,
+      -x        => -1,
+      x + 4     => 5,
+      3 + x     => 4,
+      x + y     => 3,
+      x - 1     => 0,
+      1 - x     => 0,
+      x - y     => -1,
+      -x + 3    => 2,
+      -y - x    => -3,
+      x*3       => 3,
+      4*y       => 8,
+      (+x)*(-y) => -2,
+      x/2       => 0.5,
+      y/2       => 1,
+      -2/x      => -2,
+      4/(-y)    => -2,
+      x**2      => 1,
+      4**y      => 16,
+      y**x      => 2
+    }.each_pair { |expr, value|
+      it expr do
+        expr.value.should == value
+      end
+    }
   end
 
   describe "optimization:" do
-    should_equal \
-    '-(-x)'       => 'x',
+    x = var :name => :x
+    y = var :name => :y
+    {
+      -(-x)       => x,
 
-    '0 + x'       => 'x',
-    'x + 0'       => 'x',
-    'x + (-2)'    => 'x - 2',
-    '-2 + x'      => 'x - 2',
-    '-x + 2'      => '2 - x',
-    'x + (-y)'    => 'x - y',
-    '-y + x'      => 'x - y',
+      0 + x       => x,
+      x + 0       => x,
+      x + (-2)    => x - 2,
+      -2 + x      => x - 2,
+      -x + 2      => 2 - x,
+      x + (-y)    => x - y,
+      -y + x      => x - y,
 
-    '0 - x'       => '-x',
-    'x - 0'       => 'x',
-    'x - (-2)'    => 'x + 2',
-    '-2 - (-x)'   => 'x - 2',
-    'x - (-y)'    => 'x + y',
+      0 - x       => -x,
+      x - 0       => x,
+      x - (-2)    => x + 2,
+      -2 - (-x)   => x - 2,
+      x - (-y)    => x + y,
 
-    '0 * x'       => '0',
-    'x * 0'       => '0',
-    '1 * x'       => 'x',
-    'x * 1'       => 'x',
-    '-1 * x'      => '-x',
-    'x * (-1)'    => '-x',
-    'x * (-3)'    => '-(x*3)',
-    '-3 * x'      => '-(x*3)',
-    '-3 * (-x)'   => 'x*3',
-    'x*(-y)'      => '-(x*y)',
-    '-x*y'        => '-(x*y)',
-    '(-x)*(-y)'   => 'x*y',
+      0 * x       => 0,
+      x * 0       => 0,
+      1 * x       => x,
+      x * 1       => x,
+      -1 * x      => -x,
+      x * (-1)    => -x,
+      x * (-3)    => -(x*3),
+      -3 * x      => -(x*3),
+      -3 * (-x)   => x*3,
+      x*(-y)      => -(x*y),
+      -x*y        => -(x*y),
+      (-x)*(-y)   => x*y,
 
-    '0 / x'       => '0',
-    'x / 1'       => 'x',
+      0 / x       => 0,
+      x / 1       => x,
 
-    '0**x'        => '0',
-    '1**x'        => '1',
-    'x**0'        => '1',
-    'x**1'        => 'x',
-    '(-x)**1'     => '-x',
-    '(-x)**2'     => 'x**2',
-    '(x**2)**y'   => 'x**(2*y)',
+      0**x        => 0,
+      1**x        => 1,
+      x**0        => 1,
+      x**1        => x,
+      (-x)**1     => -x,
+      (-x)**2     => x**2,
+      (x**2)**y   => x**(2*y),
 
-    'x*4*x'       => '4*x**2',
-    'x*(-1)*x**(-1)' => '-1',
-    'x**2*(-1)*x**(-1)' => '-x',
-    'x + y - x' => 'y',
-    '2*x + x**1 - y**2/y - y' => '3*x - 2*y',
-    '-(x+4)' => '-x-4',
+      x*4*x       => 4*x**2,
+      x*(-1)*x**(-1) => -1,
+      x**2*(-1)*x**(-1) => -x,
+      x + y - x => y,
+      2*x + x**1 - y**2/y - y => 3*x - 2*y,
+      -(x+4) => -x-4,
 
-    '(x/y)/(x/y)' => '1',
-    '(y/x)/(x/y)' => 'y**2/x**2'
+      (x/y)/(x/y) => 1,
+      (y/x)/(x/y) => y**2/x**2
+    }.each_pair { |non_optimized, optimized|
+      it non_optimized do
+        non_optimized.should == optimized
+      end
+    }
   end
 
   describe 'Variable methods:' do
+    let(:v) { var :name => :v }
+    let(:x) { var :name => :x, :value => 2 }
+    let(:y) { var :name => :y }
+    
     #initialize
     it 'var(:name => :v)' do
-      v = var(:name => :v)
-      [v.name, v.value].should == ['v', nil]
+      v.name.should == 'v'
+      v.value.should be nil
     end
     it 'var.to_s == "unnamed_variable"' do
-      v = var :name => nil # This is because it will auto add a name with the plugin 'autovarname'
-      [v.to_s, v.value].should == ["unnamed_variable", nil]
+      v.name = nil
+      v.to_s.should == 'unnamed_variable'
+      v.value.should be nil
     end
     it 'var init' do
-      v = var(:name => :x, :value => 2)
-      [v.name, v.value].should == ['x', 2]
+      x.value.should == 2
     end
     it 'proc value' do
-      x = var :name => :x, :value => 2
       y = var { x**2 }
       x.value = 3
       (x*y).value.should == 27
     end
 
-    x, y = var(:name => :x), var(:name => :y)
-    #variables
     it 'expression variables' do
       x.variables.should == [x]
-    end
-    it 'expression variables' do
       (-(x+y)).variables.should == [x,y]
     end
 
-    #operations
     it 'operations' do
       (-x**y-4*y+5-y/x).operations.should == {:+ => 1, :- => 2, :* => 1, :/ => 1, :-@ => 1, :** => 1}
     end
@@ -152,29 +131,37 @@ describe "Symbolic" do
     it 'math method' do
       cos = Symbolic::Math.cos(x)
       x.value = 0
-      [cos.value, cos.to_s].should == [1.0, 'cos(x)']
+      cos.value.should == 1.0
+      cos.to_s.should == 'cos(x)'
     end
   end
 
   describe "to_s:" do
-    should_print \
-    'x' => 'x',
-    '-x' => '-x',
-    'x+1' => 'x+1',
-    'x-4' => 'x-4',
-    '-x-4' => '-x-4',
-    '-(x+y)' => '-x-y',
-    '-(x-y)' => '-x+y',
-    'x*y' => 'x*y',
-    '(-x)*y' => '-x*y',
-    '(y+3)*(x+2)*4' => '4*(y+3)*(x+2)',
-    '4/x' => '4/x',
-    '2*x**(-1)*y**(-1)' => '2/(x*y)',
-    '(-(2+x))/(-(-y))' => '(-x-2)/y',
-    'x**y' => 'x**y',
-    'x**(y-4)' => 'x**(y-4)',
-    '(x+1)**(y*2)' => '(x+1)**(2*y)',
-    '-(x**y -2)+5' => '-x**y+7'
+    x = var :name => :x
+    y = var :name => :y
+    {
+      x                 => 'x',
+      -x                => '-x',
+      x+1               => 'x+1',
+      x-4               => 'x-4',
+      -x-4              => '-x-4',
+      -(x+y)            => '-x-y',
+      -(x-y)            => '-x+y',
+      x*y               => 'x*y',
+      (-x)*y            => '-x*y',
+      (y+3)*(x+2)*4     => '4*(y+3)*(x+2)',
+      4/x               => '4/x',
+      2*x**(-1)*y**(-1) => '2/(x*y)',
+      (-(2+x))/(-(-y))  => '(-x-2)/y',
+      x**y              => 'x**y',
+      x**(y-4)          => 'x**(y-4)',
+      (x+1)**(y*2)      => '(x+1)**(2*y)',
+      -(x**y-2)+5       => '-x**y+7'
+    }.each_pair { |expr, str|
+      it str do
+        expr.to_s.should == str
+      end
+    }
   end
 end
 
@@ -189,12 +176,13 @@ describe "Symbolic plugins" do
 
       it 'single variable with value' do
         x = var :value => 7
-        [x.name, x.value].should == ['x', 7]
+        x.name.should == 'x'
+        x.value.should == 7
       end
 
       it 'multiple variables' do
         x, yy, zzz = vars
-        [x.name, yy.name, zzz.name].should == ['x', 'yy', 'zzz']
+        [x, yy, zzz].map(&:name).should == ['x', 'yy', 'zzz']
       end
     end
   end
--- spec/symbolic_spec.rb
+++ spec/symbolic_spec.rb
@@ -1,52 +1,49 @@
 require File.expand_path('../spec_helper', __FILE__)
 
 describe Symbolic do
-  before(:all) do
-    @var = Object.new
-    @var.extend Symbolic
-  end
+  let(:var) { Object.new.extend Symbolic }
 
   it 'should handle unary plus' do
-    (+@var).should == @var
+    (+var).should == var
   end
 
   it 'should handle unary minus' do
-    Symbolic::Factors.should_receive(:add).with(@var, -1).and_return(:negative_value)
-    (-@var).should == :negative_value
+    Symbolic::Factors.should_receive(:add).with(var, -1).and_return(:negative_value)
+    (-var).should == :negative_value
   end
 
   it 'should handle binary plus' do
-    Symbolic::Summands.should_receive(:add).with(@var, :other_var).and_return(:result)
-    (@var + :other_var).should == :result
+    Symbolic::Summands.should_receive(:add).with(var, :other_var).and_return(:result)
+    (var + :other_var).should == :result
   end
 
   it 'should handle binary minus' do
-    Symbolic::Summands.should_receive(:subtract).with(@var, :other_var).and_return(:result)
-    (@var - :other_var).should == :result
+    Symbolic::Summands.should_receive(:subtract).with(var, :other_var).and_return(:result)
+    (var - :other_var).should == :result
   end
 
   it 'should handle multiplication' do
-    Symbolic::Factors.should_receive(:add).with(@var, :other_var).and_return(:result)
-    (@var * :other_var).should == :result
+    Symbolic::Factors.should_receive(:add).with(var, :other_var).and_return(:result)
+    (var * :other_var).should == :result
   end
 
   it 'should handle division' do
-    Symbolic::Factors.should_receive(:subtract).with(@var, :other_var).and_return(:result)
-    (@var / :other_var).should == :result
+    Symbolic::Factors.should_receive(:subtract).with(var, :other_var).and_return(:result)
+    (var / :other_var).should == :result
   end
 
   it 'should handle exponentation' do
-    Symbolic::Factors.should_receive(:power).with(@var, :other_var).and_return(:result)
-    (@var ** :other_var).should == :result
+    Symbolic::Factors.should_receive(:power).with(var, :other_var).and_return(:result)
+    (var ** :other_var).should == :result
   end
 
   it 'should delegate string representation to Printer' do
-    Symbolic::Printer.should_receive(:print).with(@var).and_return(:string)
-    @var.to_s.should == :string
+    Symbolic::Printer.should_receive(:print).with(var).and_return(:string)
+    var.to_s.should == :string
   end
 
   it 'should redefine inspect' do
-    @var.should_receive(:to_s).and_return(:string)
-    @var.inspect.should == "Symbolic(string)"
+    var.should_receive(:to_s).and_return(:string)
+    var.inspect.should == "Symbolic(string)"
   end
 end
