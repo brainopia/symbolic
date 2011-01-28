39590c01c224f3728183c44215d16f03c8932c8a
eregon <eregontp@gmail.com>
2011-01-28 12:04:58 +0100

    Improve the Summand test

--- spec/symbolic/summand_spec.rb
+++ spec/symbolic/summand_spec.rb
@@ -1,13 +1,27 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
 describe Symbolic::Summand do
-  it 'should be subclassed from Abelian' do
-    Symbolic::Summand.superclass.should == Symbolic::Abelian
+  include Symbolic
+  it 'be subclassed from Abelian' do
+    Summand.superclass.should be Abelian
   end
 
-  it 'should use identity as exponent by default' do
-    summand = Symbolic::Summand.new :base
+  it 'use identity as default power' do
+    summand = Summand.new :base
     summand.base.should == :base
-    summand.exponent.should == 0
+    summand.power.should == 1
+  end
+  
+  it 'has the specified power' do
+    summand = Summand.new :base, :power
+    summand.base.should == :base
+    summand.power.should == :power
+  end
+  
+  it 'keeps that power' do
+    x = var :name => 'x', :value => 3
+    summand = Summand.new x, 2
+    summand.power.should == 2
+    summand.value.should == 6
   end
 end
