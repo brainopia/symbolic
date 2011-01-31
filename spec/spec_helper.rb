cdce1e840c9ec1e4e5a1119d1d1a9af8ae256876
brainopia <brainopia@evilmartians.com>
2011-02-01 02:14:40 +0300

    Simplify spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,2 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-
-# Quick hacks for rspec in Ruby 1.9
-if RUBY_VERSION > "1.9"
-  class String
-    def collect(&b)
-      lines.collect(&b)
-    end
-  end
-end
-
-require 'symbolic'
 require 'rspec'
+require_relative '../lib/symbolic'
